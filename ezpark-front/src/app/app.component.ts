7a477554a4a62e167a7ad8ce33d40f149ca9bd6d
Ting Miao <tingpan666@gmail.com>
2018-05-04 00:37:40 -0400

    CHG: homepage

--- ezpark-front/src/app/app.component.ts
+++ ezpark-front/src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SigninPage } from '../pages/signin/signin';
-import { MapPage } from '../pages/map/map'
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = MapPage;
+  rootPage:any = SigninPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
